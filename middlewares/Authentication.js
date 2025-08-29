6311a45bab322dd5e0518d0e3411ea8296e93dff
Anas Khan <anaskhan995620@gmail.com>
2025-08-29 23:28:24 +0530

    update in middlewares

--- middlewares/Authentication.js
+++ middlewares/Authentication.js
@@ -1,9 +1,10 @@
-
 import jwt from "jsonwebtoken";
 
 export const authmiddleware = (req, res, next) => {
   try {
+    // sirf cookie me token check karenge
     const token = req.cookies.token;
+
     if (!token) {
       return res.status(401).json({
         success: false,
@@ -12,7 +13,7 @@ export const authmiddleware = (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { id: decoded._id };
+    req.userId = decoded._id;   // ✅ userId ko request me attach kar diya
     next();
   } catch (error) {
     return res.status(401).json({
